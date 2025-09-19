6520ac497e7e5d0284f381554f87d39488f5372a
Sic-x <137124806@QQ.COM>
2025-09-19 16:25:28 +0800

    修改名称

--- config/constants.py
+++ config/constants.py
@@ -4,7 +4,7 @@
 # @Date    : 2025/9/16
 
 # --- 插件信息 ---
-PLUGIN_NAME = "astrbot_plugin_text-renderer"
+PLUGIN_NAME = "astrbot_plugin_text_renderer"
 PLUGIN_AUTHOR = "sic"
 PLUGIN_DESCRIPTION = "文本文件生成图片"
 PLUGIN_VERSION = "1.1.0"
